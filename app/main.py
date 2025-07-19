7a91c070fc407c91ece21f08d7886e129c73db59
Vovas <vovaafon25@gmail.com>
2025-07-19 18:37:15 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -9,18 +9,18 @@ def shop_trip() -> None:
     for customer in customers_list:
         print(f"{customer.name} has {customer.money} dollars")
 
-        min_cost = None
+        minimal_cost = None
         shop_to_go = None
 
         for shop in shops_list:
             cost = customer.calculate_trip_price(shop, fuel_price)
             print(f"{customer.name}'s trip to "
                   f"the {shop.name} costs{cost: 0.2f}")
-            if min_cost is None or cost < min_cost:
-                min_cost = cost
+            if minimal_cost is None or cost < minimal_cost:
+                minimal_cost = cost
                 shop_to_go = shop
 
-        if customer.money < min_cost:
+        if customer.money < minimal_cost:
             print(f"{customer.name} doesn't have enough money "
                   f"to make a purchase in any shop")
             continue
@@ -29,7 +29,7 @@ def shop_trip() -> None:
         customer.buy_products(shop_to_go)
 
         print(f"{customer.name} rides home")
-        amount_left = round(customer.money - min_cost, 2)
+        amount_left = round(customer.money - minimal_cost, 2)
         print(f"{customer.name} now has {amount_left} dollars", end="\n\n")
 
 
